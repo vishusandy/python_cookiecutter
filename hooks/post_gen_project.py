e7c17cad2e367e0447fa1a326c1227742fb473eb
Andrew <kaioshin2005@yahoo.com>
2023-07-25 10:10:00 -0500

    remove LICENSE file if no license selected

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,6 +1,11 @@
+import os
 import subprocess
 
 git = {{ cookiecutter.git_init }}
+license = '{{ cookiecutter.license }}'
 
 if git:
     subprocess.run(['git', 'init', '-q', '-b', 'main'])
+
+if license == '' or license.lower() == 'none':
+    os.remove('LICENSE')
